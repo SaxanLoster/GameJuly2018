2e08cc07a7c163de0bdc6cca67cae1de3bb724ae
Saxan Loster <yawgmoth@linkline.com>
2018-07-12 13:40:05 -0700

    touch movement prototype

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+  public Transform player;
+  private Vector3 offset = new Vector3( 0f , 2f , -10f );
+
+  void Start () {
+    
+  }
+
+  void Update () {
+    transform.position = player.position + offset;
+    }
+  }
--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 
 public class PlayerControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+  private float moveSpeed = 10f;
+  private bool mousemoved = false;
+  private float moveX = 0;
+  private float moveY = 0;
+  public Rigidbody rb;
+
+  void Start () {}
+
+  void OnGUI () {
+    Event ev = Event.current;
+    if (ev.type == EventType.MouseUp) {
+      moveX = 0;
+      moveY = 0;
+      mousemoved = false;
+      }
+    else if ( !mousemoved && ev.type == EventType.MouseDrag ) {
+      moveX = ev.delta[ 0 ] != 0 ? ( ev.delta[ 0 ] > 0 ? 1 : -1 ) : 0;
+      moveY = ev.delta[ 1 ] != 0 ? ( ev.delta[ 1 ] > 0 ? 1 : -1 ) : 0;
+      mousemoved = true;
+      }
+    }
+
+  void FixedUpdate () {
+    float transX = 0f;
+    float transY = 0f;
+    float transZ = 0f;
+    if ( moveX != 0 ) transX = moveSpeed * moveX * Time.deltaTime;
+    if ( moveY != 0 ) transY = moveSpeed * moveY * Time.deltaTime;
+    transZ = moveSpeed * Time.deltaTime;
+    transform.Translate( transX , transY , transZ );
+    }
+
+  void Update () {}
+
+  }
